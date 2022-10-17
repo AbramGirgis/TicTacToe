715110dd804854f75031c4b340ef8ab43d6c4cea
AbramGirgis <girgis.abram@gmail.com>
2022-10-16 23:46:28 -0400

    computer input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    static Scanner in = new Scanner(System.in);
     static char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
@@ -13,55 +15,76 @@ public class Main {
         //Print the game board
         printGameBoard(gameBoard);
 
-        Scanner in = new Scanner(System.in);
+        boolean check = true;
+        while (check) {
+            // Player input
+            System.out.println("Player1 turn...");
+            player1Move();
 
-        // Player input
-        System.out.print("Enter your placement (1-9): ");
-        int playerPosition = in.nextInt();
-        System.out.println(playerPosition);
-        placePiece(playerPosition, 'X');
-
-
+            //Computer input (random)
+            System.out.println("\nComputer turn...");
+            playerComputerMove();
+        }
 
     }
 
-    private static void placePiece(int playerPosition, char x) {
-        switch (playerPosition){
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-            case 3:
-                gameBoard[0][4] = 'X';
-                break;
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-            case 6:
-                gameBoard[2][4] = 'X';
-                break;
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-            case 9:
-                gameBoard[4][4] = 'X';
-                break;
-            default:
-                System.out.println("Wrong choice! please choose a number from 1 to 9!");
-        }
+    private static void playerComputerMove() {
+        Random rand = new Random();
+        int computerPosition = rand.nextInt(9) + 1;
+        placePiece(computerPosition, 'O');
         printGameBoard(gameBoard);
     }
 
+    private static void player1Move() {
+        System.out.print("Enter your placement (1-9): ");
+        int playerPosition = in.nextInt();
+        if (playerPosition > 9 || playerPosition < 1) {
+            System.out.println("Wrong choice! please choose a number from 1 to 9!");
+            player1Move();
+        } else {
+            System.out.println(playerPosition);
+            placePiece(playerPosition, 'X');
+            printGameBoard(gameBoard);
+        }
+    }
+
+    private static void placePiece(int position, char c) {
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = c;
+                break;
+            case 2:
+                gameBoard[0][2] = c;
+                break;
+            case 3:
+                gameBoard[0][4] = c;
+                break;
+            case 4:
+                gameBoard[2][0] = c;
+                break;
+            case 5:
+                gameBoard[2][2] = c;
+                break;
+            case 6:
+                gameBoard[2][4] = c;
+                break;
+            case 7:
+                gameBoard[4][0] = c;
+                break;
+            case 8:
+                gameBoard[4][2] = c;
+                break;
+            case 9:
+                gameBoard[4][4] = c;
+                break;
+            default:
+                break;
+        }
+
+    }
+
     private static void printGameBoard(char[][] gameBoard) {
-        for (char[] row : gameBoard){
+        for (char[] row : gameBoard) {
             for (char c : row) {
                 System.out.print(c);
             }
