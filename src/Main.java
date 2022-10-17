0f3fdae035ba86b212a6c3365700a0b83f70b243
AbramGirgis <girgis.abram@gmail.com>
2022-10-17 00:22:50 -0400

    completed the game!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -10,20 +9,36 @@ public class Main {
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '}};
 
+    static ArrayList<Integer> playerPositions = new ArrayList<>();
+    static ArrayList<Integer> computerPositions = new ArrayList<>();
+
     public static void main(String[] args) {
 
         //Print the game board
         printGameBoard(gameBoard);
 
-        boolean check = true;
-        while (check) {
+        while (true) {
             // Player input
-            System.out.println("Player1 turn...");
+            System.out.println("\nPlayer1 turn...");
             player1Move();
+
+            //Check if player wins!
+            String result = checkWinner();
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
 
             //Computer input (random)
             System.out.println("\nComputer turn...");
             playerComputerMove();
+
+            //Check if computer wins!
+            result = checkWinner();
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
         }
 
     }
@@ -31,8 +46,13 @@ public class Main {
     private static void playerComputerMove() {
         Random rand = new Random();
         int computerPosition = rand.nextInt(9) + 1;
-        placePiece(computerPosition, 'O');
-        printGameBoard(gameBoard);
+        if (playerPositions.contains(computerPosition) || computerPositions.contains(computerPosition)) {
+            playerComputerMove();
+        } else {
+            placePiece(computerPosition, 'O');
+            computerPositions.add(computerPosition);
+            printGameBoard(gameBoard);
+        }
     }
 
     private static void player1Move() {
@@ -41,9 +61,13 @@ public class Main {
         if (playerPosition > 9 || playerPosition < 1) {
             System.out.println("Wrong choice! please choose a number from 1 to 9!");
             player1Move();
+        } else if (playerPositions.contains(playerPosition) || computerPositions.contains(playerPosition)) {
+            System.out.println("Position is taken! please enter a correct position!");
+            player1Move();
         } else {
             System.out.println(playerPosition);
             placePiece(playerPosition, 'X');
+            playerPositions.add(playerPosition);
             printGameBoard(gameBoard);
         }
     }
@@ -90,6 +114,39 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static String checkWinner(){
+        List topRow = Arrays.asList(1,2,3);
+        List midRow = Arrays.asList(4,5,6);
+        List bottomRow = Arrays.asList(7,8,9);
+        List leftCol = Arrays.asList(1,4,7);
+        List midCol = Arrays.asList(2,5,8);
+        List rightCol = Arrays.asList(3,6,9);
+        List diagonal1 = Arrays.asList(1,5,9);
+        List diagonal2 = Arrays.asList(3,5,7);
+
+        List<List> winningConditions = new ArrayList<List>();
+        winningConditions.add(topRow);
+        winningConditions.add(midRow);
+        winningConditions.add(bottomRow);
+        winningConditions.add(leftCol);
+        winningConditions.add(midCol);
+        winningConditions.add(rightCol);
+        winningConditions.add(diagonal1);
+        winningConditions.add(diagonal2);
+
+        for (List l : winningConditions){
+            if (playerPositions.containsAll(l)){
+                return "Congratulations, you won!";
+            } else if (computerPositions.containsAll(l)) {
+                return "Computer wins! Sorry :(";
+            } else if ((playerPositions.size() + computerPositions.size()) == 9){
+                return "Draw!";
+            }
+        }
+
+        return "";
     }
 
 }
